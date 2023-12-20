f397be2163fce96142d70d697a04d73af8031ebc
pramathvb12 <106541314+pramathvb12@users.noreply.github.com>
2023-12-20 19:41:59 +0530

    Add files via upload

---
+++ Day13.py
@@ -0,0 +1,26 @@
+'''
+character to ASCII & ASCII to character 
+'''
+# Character to ASCII
+char_value = 'A'
+ascii_value = ord(char_value)
+print(f"Character to ASCII: {ascii_value}")
+
+# ASCII to Character
+ascii_value = 65
+char_value = chr(ascii_value)
+print(f"ASCII to Character: {char_value}")
+
+'''
+String to int & int to str conversion
+'''
+# Integer to String
+integer_value = 123
+string_value = str(integer_value)
+print(f"Integer to String: {string_value}")
+
+# String to Integer
+string_value = "456"
+integer_value = int(string_value)
+print(f"String to Integer: {integer_value}")
+
